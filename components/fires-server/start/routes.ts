1ed2e6fffd36e4d423eeac8b57398cfb7dc935c0
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2025-05-08 22:02:41 +0200

    Improve readability of routes file

--- components/fires-server/start/routes.ts
+++ components/fires-server/start/routes.ts
@@ -9,27 +9,27 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.get('/', [() => import('#controllers/about_controller'), 'index']).as('about')
+const AboutController = () => import('#controllers/about_controller')
+const NodeinfoController = () => import('#controllers/well-known/nodeinfo_controller')
+const LabelsController = () => import('#controllers/labels_controller')
+const LabelsApiController = () => import('#controllers/api/labels_controller')
+
+router.get('/', [AboutController, 'index']).as('about')
 router.get('/health', ({ response }) => {
   return response.send('ok')
 })
 
 // NodeInfo
-const nodeinfoController = () => import('#controllers/well-known/nodeinfo_controller')
-
-router.get('/.well-known/nodeinfo', [nodeinfoController, 'discovery']).as('nodeinfo.discovery')
-router.get('/nodeinfo/2.1', [nodeinfoController, 'retrieval']).as('nodeinfo.retrieval')
+router.get('/.well-known/nodeinfo', [NodeinfoController, 'discovery']).as('nodeinfo.discovery')
+router.get('/nodeinfo/2.1', [NodeinfoController, 'retrieval']).as('nodeinfo.retrieval')
 
 // FIRES labels Endpoint
-router.resource('labels', () => import('#controllers/labels_controller')).only(['index', 'show'])
+router.resource('labels', LabelsController).only(['index', 'show'])
 
 // Reference Server Management API:
 router
   .group(() => {
-    router
-      .resource('labels', () => import('#controllers/api/labels_controller'))
-      .except(['create', 'edit'])
-      .as('labels')
+    router.resource('labels', LabelsApiController).except(['create', 'edit']).as('labels')
   })
   .prefix('api')
   .as('api')

849f561c422462aa22b9852df90c049d3fd086fb
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2025-07-10 19:55:39 +0200

    Remove caching from description and other settings

--- components/fires-server/app/controllers/about_controller.ts
+++ components/fires-server/app/controllers/about_controller.ts
@@ -1,13 +1,10 @@
 import Setting from '#models/setting'
 import markdown from '#utils/markdown'
-import cache from '@adonisjs/cache/services/main'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AboutController {
   async index({ view }: HttpContext) {
     const settings = await this.getSettings()
-
-    console.log(typeof settings.description, settings)
 
     return view.render('about/index', {
       description: settings.description,
@@ -16,26 +13,19 @@ export default class AboutController {
     })
   }
 
-  private getSettings() {
-    return cache.getOrSet({
-      key: `settings:about`,
-      tags: ['settings'],
-      factory: async () => {
-        const settings = await Setting.retrieveSettings([
-          'description',
-          'documentation_url',
-          'appeals_url',
-        ])
+  private async getSettings() {
+    const settings = await Setting.retrieveSettings([
+      'description',
+      'documentation_url',
+      'appeals_url',
+    ])
 
-        return {
-          description: settings.description
-            ? markdown.render(settings.description)
-            : 'This FIRES server is not yet configured correctly.',
-          documentation_url: settings.documentation_url ?? null,
-          appeals_url: settings.appeals_url ?? null,
-        }
-      },
-      ttl: '60m',
-    })
+    return {
+      description: settings.description
+        ? markdown.render(settings.description)
+        : 'This FIRES server is not yet configured correctly.',
+      documentation_url: settings.documentation_url ?? null,
+      appeals_url: settings.appeals_url ?? null,
+    }
   }
 }

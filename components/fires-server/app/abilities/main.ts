987e976eaad392292c78a1f16ddb30d2b4ae3c7f
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2025-11-29 18:21:58 +0100

    Add @adonisjs/bouncer package for authorization

---
+++ components/fires-server/app/abilities/main.ts
@@ -0,0 +1,19 @@
+/*
+|--------------------------------------------------------------------------
+| Bouncer abilities
+|--------------------------------------------------------------------------
+|
+| You may export multiple abilities from this file and pre-register them
+| when creating the Bouncer instance.
+|
+| Pre-registered policies and abilities can be referenced as a string by their
+| name. Also they are must if want to perform authorization inside Edge
+| templates.
+|
+*/
+
+// import { Bouncer } from '@adonisjs/bouncer'
+
+// export const editUser = Bouncer.ability(() => {
+//   return true
+// })
